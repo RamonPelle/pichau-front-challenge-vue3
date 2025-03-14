36e4f0e77dd6c5538f8f3c83d6ed84ca61e73498
Matheus Passos <vpsmatheus@hotmail.com>
2025-03-14 08:20:13 -0300

    start project

---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <h1>Desafio Vue.js - Tabela de Comparação de Preços</h1>
+    <p>Implemente o componente <code>PriceComparisonTable</code> conforme as especificações.</p>
+  </div>
+</template>
+
+<script>
+export default { name: 'HomeView' };
+</script>
